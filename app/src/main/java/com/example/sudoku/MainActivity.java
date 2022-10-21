182d7dabde4fbc15bc74965c08f2db6629abb3a4
Rafael-Puerta <rafapc1992@gmail.com>
2022-10-21 19:22:00 +0200

    Toast implementado

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.sudoku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +23,33 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<9;i++){
             TableRow rowy=new TableRow(this);
             for(int j=0;j<9;j++){
+
                 Spinner sp=new Spinner(this);
                 sp.setBackground(null);
                 sp.setPadding(5, 5, 5, 5);
+                sp.setTag(R.id.col,j);
+                sp.setTag(R.id.fila,i);
+                sp.setTag("bug init");
+
+                sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int k, long l) {
+                        // la posició del spinner és 'i', però també es pot buscar amb
+                        int fila = (int) adapterView.getTag(R.id.fila);
+                        int col = (int) adapterView.getTag(R.id.col);
+                        if (sp.getTag().equals("bug init")){
+                            sp.setTag("guay");
+                            return;
+                        }
+                        Toast.makeText(MainActivity.this, "fila: "+fila+" columna: "+col+" nuevo valor: "+k, Toast.LENGTH_SHORT).show();
+
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        // Another interface callback
+                    }
+                });
+
+
                 ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
                         android.R.layout.simple_spinner_item, nombres);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -31,6 +58,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
             tablita.addView(rowy);
+
         }
+
     }
+
 }
